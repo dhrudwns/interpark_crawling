9c90953f814e873a816d4fef5f4acb1af1311b73
dhrudwns <dhrudwns12@nvaer.com>
2018-12-05 11:52:34 +0900

    project

---
+++ book_crawl.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class BookCrawlSpider(CrawlSpider):
+    name = 'book_crawl'
+    allowed_domains = ['hanbit.co.kr']
+    start_urls = ['http://hanbit.co.kr/']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        i = {}
+        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
+        #i['name'] = response.xpath('//div[@id="name"]').extract()
+        #i['description'] = response.xpath('//div[@id="description"]').extract()
+        return i
